fba197b1d6d34cee293f20683e98ac2958c8dea6
Ashish Paul <AshishPaul@users.noreply.github.com>
2024-04-05 10:52:06 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
 group = "com.zerogravity.moonlight"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenLocal()
-}
-
 catalog {
     versionCatalog {
         from(files("./gradle/moonlight.versions.toml"))
